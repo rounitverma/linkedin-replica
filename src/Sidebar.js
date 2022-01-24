1f177afd315c740a8378a622e6afbcf70453d77d
Rounit Verma <rounitanand@gmail.com>
2022-01-24 23:46:48 +0530

    header done

---
+++ src/Sidebar.js
@@ -0,0 +1,36 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img
+          src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max"
+          alt=""
+        />
+        <Avatar className="sidebar_avatar" />
+        <h2>Rounit Verma</h2>
+        <h4>rounitanand@gmail.com</h4>
+      </div>
+
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebar_statNumber">2,543</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar_statNumber">2,448</p>
+        </div>
+      </div>
+
+      <div className="sidebar_bottom">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
